0bbb96bc1f942777861a17f149ec09845a6a0eef
Elisafan Wathuo <elisafan.wathuo@turnkeyafrica.com>
2018-02-19 20:42:39 +0300

    L-63

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return(
-        <div>
+        <div className="Person">
         <p onClick={props.click}>i am {props.name} and i am {props.age}</p>
         <p>{props.children}</p>
-        <input type="text" onChange={props.changed} />
+        <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
 }
