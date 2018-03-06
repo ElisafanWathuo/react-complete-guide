693283b4ed274097f771157faa1374957d4ec8ba
ewathuo <ewathuo@gmail.com>
2018-03-06 07:54:59 +0300

    L-91

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Person from '../components/Persons/Person/Person';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-class App extends Component {
+class App extends PureComponent {
     constructor(props){
         super(props);
         console.log('[App js] Inside Constructor', props);
@@ -17,6 +17,21 @@ class App extends Component {
     componentDidMount(){
         console.log('[App js] Inside componentDidMount()');
     }
+
+    //shouldComponentUpdate (nextProps, nextState) {
+    //    console.log('[UPDATE App js] Inside shouldComponentUpdate()', nextProps, nextState);
+    //    return nextState.persons !== this.state.persons||
+    //    nextState.showPersons !== this.state.showPersons;
+    //}
+
+    componentWillUpdate (nextProps, nextState) {
+        console.log('[UPDATE App js] Inside componentWillUpdate()', nextProps, nextState);
+    }
+
+    componentDidUpdate () {
+        console.log('[UPDATE App js] Inside componentDidUpdate()');
+    }
+
     state = {
         persons: [
             {id:'tudfb', name: 'Max', age: 28},
@@ -58,22 +73,23 @@ class App extends Component {
   render() {
     console.log('[App js] Inside render()');
       let persons = null;
-      
+
 
       if(this.state.showPersons){
-          persons = 
+          persons =
                   <Persons
                     persons={this.state.persons}
                     clicked={this.deletePersonHandler}
                     changed={this.nameChangedHandler} />;
-           
+
       }
 
-      
+
 
     return (
       <div className={classes.App}>
-        <Cockpit 
+        <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
+        <Cockpit
         showPersons={this.state.showPersons}
         persons={this.state.persons}
         clicked={this.togglePersonsHandler}
